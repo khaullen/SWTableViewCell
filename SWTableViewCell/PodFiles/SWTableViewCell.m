6e051c72b5970e75df45e680ae1d9c11a56622cc
Phil Jacobsen <philjacobsen@gmail.com>
2014-05-21 17:43:35 -0400

    Temporary fix for iOS 6 crash.

--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -8,6 +8,7 @@
 
 #import "SWTableViewCell.h"
 #import "SWUtilityButtonView.h"
+#import "UITableViewCell+FixUITableViewCellAutolayout.h"
 
 #define kSectionIndexWidth 15
 #define kAccessoryTrailingSpace 15
@@ -278,7 +279,6 @@ static NSString * const kTableViewPanState = @"state";
 
 - (void)layoutSubviews
 {
-    [super layoutSubviews];
     
     // Offset the contentView origin so that it appears correctly w/rt the enclosing scroll view (to which we moved it).
     CGRect frame = self.contentView.frame;
@@ -293,6 +293,8 @@ static NSString * const kTableViewPanState = @"state";
     }
     
     [self updateCellState];
+    [super layoutSubviews];
+
 }
 
 - (void)prepareForReuse
